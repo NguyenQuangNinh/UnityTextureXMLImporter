455a0e5020ad779af630439cafff214c2f4570b9
NinhNQ <ninhnq@568e.vn>
2016-07-19 16:02:50 +0700

    ~ Implement Slicer

--- Assets/Editor/TextureAtlasSlicer.cs
+++ Assets/Editor/TextureAtlasSlicer.cs
@@ -1,4 +1,7 @@
-﻿using UnityEditor;
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEditor;
 using UnityEngine;
 
 public class TextureAtlasSlicer : EditorWindow {
@@ -56,9 +59,111 @@ public class TextureAtlasSlicer : EditorWindow {
 		}
 
 		if (GUILayout.Button("Slice")) {
-			//PerformSlice();
+			PerformSlice();
 		}
 
 		GUI.enabled = enabled;
 	}
+    private void PerformSlice()
+    {
+        XmlDocument document = new XmlDocument();
+        document.LoadXml(xmlAsset.text);
+
+        XmlElement root = document.DocumentElement;
+        if (root.Name == "TextureAtlas")
+        {
+            bool failed = false;
+
+            Texture2D texture = AssetDatabase.LoadMainAssetAtPath(importer.assetPath) as Texture2D;
+            int textureHeight = texture.height;
+
+            List<SpriteMetaData> metaDataList = new List<SpriteMetaData>();
+
+            foreach (XmlNode childNode in root.ChildNodes)
+            {
+                if (childNode.Name == "SubTexture")
+                {
+                    try
+                    {
+                        int width = Convert.ToInt32(childNode.Attributes["width"].Value);
+                        int height = Convert.ToInt32(childNode.Attributes["height"].Value);
+                        int x = Convert.ToInt32(childNode.Attributes["x"].Value);
+                        int y = textureHeight - (height + Convert.ToInt32(childNode.Attributes["y"].Value));
+
+                        SpriteMetaData spriteMetaData = new SpriteMetaData
+                        {
+                            alignment = (int)spriteAlignment,
+                            border = new Vector4(),
+                            name = childNode.Attributes["name"].Value,
+                            pivot = GetPivotValue(spriteAlignment, customOffset),
+                            rect = new Rect(x, y, width, height)
+                        };
+
+                        metaDataList.Add(spriteMetaData);
+                    }
+                    catch (Exception exception)
+                    {
+                        failed = true;
+                        Debug.LogException(exception);
+                    }
+                }
+                else
+                {
+                    Debug.LogError("Child nodes should be named 'SubTexture' !");
+                    failed = true;
+                }
+            }
+
+            if (!failed)
+            {
+                importer.spriteImportMode = SpriteImportMode.Multiple;
+                importer.spritesheet = metaDataList.ToArray();
+
+                EditorUtility.SetDirty(importer);
+
+                try
+                {
+                    AssetDatabase.StartAssetEditing();
+                    AssetDatabase.ImportAsset(importer.assetPath);
+                }
+                finally
+                {
+                    AssetDatabase.StopAssetEditing();
+                    Close();
+                }
+            }
+        }
+        else
+        {
+            Debug.LogError("XML needs to have a 'TextureAtlas' root node!");
+        }
+    }
+    public static Vector2 GetPivotValue(SpriteAlignment alignment, Vector2 customOffset)
+    {
+        switch (alignment)
+        {
+            case SpriteAlignment.Center:
+                return new Vector2(0.5f, 0.5f);
+            case SpriteAlignment.TopLeft:
+                return new Vector2(0.0f, 1f);
+            case SpriteAlignment.TopCenter:
+                return new Vector2(0.5f, 1f);
+            case SpriteAlignment.TopRight:
+                return new Vector2(1f, 1f);
+            case SpriteAlignment.LeftCenter:
+                return new Vector2(0.0f, 0.5f);
+            case SpriteAlignment.RightCenter:
+                return new Vector2(1f, 0.5f);
+            case SpriteAlignment.BottomLeft:
+                return new Vector2(0.0f, 0.0f);
+            case SpriteAlignment.BottomCenter:
+                return new Vector2(0.5f, 0.0f);
+            case SpriteAlignment.BottomRight:
+                return new Vector2(1f, 0.0f);
+            case SpriteAlignment.Custom:
+                return customOffset;
+            default:
+                return Vector2.zero;
+        }
+    }
 }
